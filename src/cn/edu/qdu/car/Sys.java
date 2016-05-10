92e551075553858a59e966e6e369605cf7d7efd2
183107439 <cys_2017@163.com>
2016-05-10 17:13:55 +0800

    从新修改

--- src/cn/edu/qdu/car/Sys.java
+++ src/cn/edu/qdu/car/Sys.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class Sys {
 	//属性
-//	Vehicles[] vehicles;
-//	private double totalFee=0;
+	Vehicles[] vehicles;
+
 	//静态块
 	//初始化块
 	//构造方法
@@ -15,70 +15,43 @@ public class Sys {
 		String[] carBrand={"别克","宝马","奥迪"};  //汽车品牌
 		int[] coacheSeatNumber={16,32};		//客车座位数
 		Scanner input=new Scanner(System.in);
+		System.out.println("请问您想租多少辆车？");
+		int count=input.nextInt();
+		//Vehicles[] vehicles;
+		int[] rentedDays=new int[count];
+		
+		int carNo=11111;
+		int busNo=55555;
 		double totalFee=0;
-		String key;
-		do{
+		String key;	
+		
+		for (int i = 0; i < count; i++) {
 			System.out.println("请问您想租什么车？");
 			System.out.println("1.汽车\t2.客车");
 			int choice1=input.nextInt();
 			if(choice1==1){
-				int carNo=11111;
-				System.out.println("请问您想租多少辆车？");
-				int count=input.nextInt();
-				Vehicles[] vehicles=new Car[count];  //多态
-				int[] rentedDays=new int[count];
-				for (int i = 0; i < vehicles.length; i++) {
-					System.out.println("请选择汽车的品牌：");
-					System.out.println("1.别克\t2.宝马\t3.奥迪");
-					int choice2=input.nextInt();
-					//设置汽车品牌、车牌号
-					String licensePlateNumber="鲁B"+(carNo+i);
-					vehicles[i]=new Car(licensePlateNumber,carBrand[choice2-1]);  //多态   					
-//					((Car)vehicles[i]).setBrand(carBrand[choice2-1]); //可行但不建议这么做
-					//设置租车天数
-					System.out.println("请问您想租多少天？");
-					rentedDays[i]=input.nextInt();
-					//总租金增加
-					//totalFee=totalFee+vehicles[i].rentFee(rentedDays);
-					totalFee=totalFee+rentFee(vehicles[i],rentedDays[i],totalFee);
-				}
-				System.out.println("您所租的汽车有：");
-				System.out.println("品牌\t日租金\t租车天数\t车牌号");
-				for (int i = 0; i < vehicles.length; i++) {
-					vehicles[i].show(rentedDays[i]);
-				}
-				//System.out.println("租金一共："+totalFee);
+				System.out.println("请选择汽车的品牌：");
+				System.out.println("1.别克\t2.宝马\t3.奥迪");
+				int choice2=input.nextInt();
+				String lincensePlateNumber="鲁B"+carNo;
+				vehicles[i]=new Car(lincensePlateNumber,carBrand[choice2-1]);
+				System.out.println("请问您想租多少天？");
+				rentedDays[i]=input.nextInt();
+				totalFee=totalFee+rentFee(vehicles[i],rentedDays[i],totalFee);
+				carNo++;
 			}else{
-				int busNo=55555;
-				System.out.println("请问您想租多少辆车？");
-				int count=input.nextInt();
-				Vehicles[] vehicles=new Coache[count];	//多态
-				int[] rentedDays=new int[count];
-				for (int i = 0; i < vehicles.length; i++) {
-					System.out.println("请选择客车类型：");
-					System.out.println("1. <=16座\t 2. >16座");
-					int choice2=input.nextInt();
-					//设置客车类型、车牌号
-					String licensePlateNumber="鲁A"+(busNo+i);
-					vehicles[i]=new Coache(licensePlateNumber,coacheSeatNumber[choice2-1]);	//多态					
-//					((Coache)vehicles[i]).setSeatNumber(coacheSeatNumber[choice2-1]); //可行但不建议这么做
-					//设置租车天数
-					System.out.println("请问您想租多少天？");
-					rentedDays[i]=input.nextInt();
-					//总租金增加
-					//totalFee=totalFee+vehicles[i].rentFee(rentedDays);
-					totalFee=totalFee+rentFee(vehicles[i],rentedDays[i],totalFee);
-				}
-				System.out.println("您所租的客车有：");
-				System.out.println("品牌\t座位数\t租车天数\t车牌号");
-				for (int i = 0; i < vehicles.length; i++) {
-					vehicles[i].show(rentedDays[i]);
-				}
-				//System.out.println("租金一共："+totalFee);
-			}	
-			System.out.println("是否继续租车？(y/n)");
-			key=input.next();
-		}while(key.equalsIgnoreCase("y"));
+				System.out.println("请问您想多少座位数的客车：");
+				System.out.println("1.<=16座\t2.>16座");
+				int choice2=input.nextInt();
+				String licensePlateNumber="鲁A"+busNo;
+				vehicles[i]=new Coache(licensePlateNumber,coacheSeatNumber[choice2-1]);
+				System.out.println("请问您想租多少天？");
+				rentedDays[i]=input.nextInt();
+				totalFee=totalFee+rentFee(vehicles[i],rentedDays[i],totalFee);
+			}
+		}		
+		//显示所租的所有车辆
+		showAllRentedVehicles(vehicles,rentedDays);
 		//输出总租金
 		System.out.println("租金一共："+totalFee+"元");
 	}
@@ -93,7 +66,7 @@ public class Sys {
 			}else{
 				vehicle.setDayRates(600);
 			}
-			//return vehicle.getDayRates()*days;
+			
 		}
 		else{
 			if(((Coache)vehicle).getSeatNumber()<=16){
@@ -101,9 +74,21 @@ public class Sys {
 			}else{
 				vehicle.setDayRates(1500);
 			}
-			//return 
+			 
 		}
 		return vehicle.getDayRates()*days;
 	}
 	
+	//显示所租的所有车辆
+	public void showAllRentedVehicles(Vehicles[] vehicles,int[] rentedDays){
+		for (int i = 0; i < vehicles.length; i++) {
+			if (vehicles[i] instanceof Car) {
+				System.out.print("汽车\t");
+				vehicles[i].show(rentedDays[i]);				
+			}else{
+				System.out.print("客车\t");
+				vehicles[i].show(rentedDays[i]);
+			}
+		}
+	}
 }
